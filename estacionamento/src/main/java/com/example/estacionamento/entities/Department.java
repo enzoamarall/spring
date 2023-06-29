cd90d6e81749809e6c8397c2f60425f5673453b0
Enzo Amaral <111314488+enzoamarall@users.noreply.github.com>
2023-06-29 11:17:28 -0300

    Add files via upload

--- estacionamento/src/main/java/com/example/estacionamento/entities/Department.java
+++ estacionamento/src/main/java/com/example/estacionamento/entities/Department.java
@@ -8,14 +8,19 @@ import jakarta.persistence.*;
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
 
-    public long getId() {
+
+    public Department(){
+
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
